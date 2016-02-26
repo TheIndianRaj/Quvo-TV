cc6da3e11e39125eb9da5e0d0d4bbbdacf9a51d3
TheIndianRaj <ark.raj2010@gmail.com>
2016-02-27 02:50:14 +0530

    Add Video Frontend Completed

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ var path = require ('path');
 
 app.set('views', path.join(__dirname + '/public/views'));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
 	res.render('index');
+});
+
+app.get('/videos', function(req, res){
+	res.render('videos');
+});
+
+app.get('/videos/add', function(req, res){
+	res.render('addVideo');
 });
 
 app.listen(PORT, function(){
