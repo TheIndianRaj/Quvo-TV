09cffef0d9561d051856d72bf490d1908df41c3b
TheIndianRaj <ark.raj2010@gmail.com>
2016-03-06 07:39:45 +0530

    Video adding almost complete. Final touches yet to be done

---
+++ config/routes.js
@@ -0,0 +1,396 @@
+var express = require('express');
+var _ = require('underscore');
+var app = express();
+var bodyParser = require('body-parser');
+var db = require('./db.js');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.get('/videos', function(req, res){
+	res.render('videos');
+});
+
+app.get('/videos/add', function(req, res){
+	var db = require('../config/db.js');
+	
+	db.query('CALL getDropdownOptions()', function(err, rows, fields) {
+
+		var tables = [
+			'types',
+			'videos',
+			'teams', 
+			'artists',
+			'genres', 
+			'series', 
+			'tags', 
+			'social_media_tags'
+		],
+		i = 0,
+		options = new Array();
+
+		if (!err){
+			rows.forEach(function(items){
+				table = tables[i++];
+				var column = 'name';
+				if(table === 'videos'){
+					column = 'title';
+				}
+				if(table !== undefined){
+					for(var row in items){
+						var obj = {
+							table: table,
+							id: items[row]["id"],
+							name: items[row][column]
+						};
+						options.push(obj);
+					}
+				}
+			});
+					 
+			res.render('addVideo', { dropdown: options});
+		}
+		else{
+			console.log(err);
+		}
+	});
+		
+});
+
+app.post('/videos/add', function(req, res){
+	var fields = ['type', 'title', 'description', 'team', 'artists', 'genres', 'imdbUrl', 'rottenTomatoesUrl', 'ageRestriction', 'tags', 'socialMediaTags', 'youtubeUrl', 'seriesId', 'seriesSeason', 'seriesEpisode'];
+	var db = require('../config/db.js');
+
+	var data = _.pick(req.body, function(value, key, data){
+		if(fields.indexOf(key) !== -1){
+			return key;
+		}
+	});
+
+	if (data.hasOwnProperty('youtubeCheckbox')) {
+		if(data.youtubeCheckbox !== 'on'){
+			data.youtubeUrl = null;
+		}
+	}
+
+	if (data.hasOwnProperty('seriesCheckbox')) {
+		if(data.seriesCheckbox === 'on'){
+			data.seriesId = null; 
+			data.seriesSeason = null; 
+			data.seriesEpisode = null;
+		}
+	}
+
+	data.artistIds = [];
+	data.tagIds = [];
+	data.socialMediaTagIds = [];
+
+	var flag = {
+		artists : (data.hasOwnProperty('artists')) ? false: true,
+		tags: (data.hasOwnProperty('tags')) ? false: true,
+		socialMediaTags: (data.hasOwnProperty('socialMediaTags')) ? false: true
+	};
+
+	
+
+	if (data.hasOwnProperty('artists') && !_.isEmpty(data.artists)) {
+		if(typeof data.artists === 'string')
+			data.artists = new Array(data.artists);
+		data.artists.forEach(function(artist, key){
+			artist = artist.trim();
+			var isNum = /^\d+$/.test(artist);
+			if(!isNum && !_.isEmpty(artist)){
+				db.query('SET  @artistId= -1; CALL insertArtist("'+artist+'", @artistId); SELECT @artistId;', function(err, rows, fields) {
+
+					if (!err){
+						storeId(rows[2][0], 'artist').then(function(){
+							checkInsertVideo();
+						});
+
+					}
+					else{
+						console.log(err);
+					}
+				});
+			}
+			else if(isNum){
+				data.artistIds.push(parseInt(artist));
+			}
+		});
+	}
+
+	if (data.hasOwnProperty('tags') && !_.isEmpty(data.tags)) {
+		if(typeof data.tags === 'string')
+			data.tags = new Array(data.tags);
+		data.tags.forEach(function(tag, key){
+			tag = tag.trim();
+			var isNum = /^\d+$/.test(tag);
+			if(!isNum && !_.isEmpty(tag)){
+				db.query('SET  @tagId= -1; CALL insertTag("'+tag+'", @tagId); SELECT @tagId;', function(err, rows, fields) {
+
+					if (!err){
+						storeId(rows[2][0], 'tag').then(function(){
+							checkInsertVideo();
+						});
+
+					}
+					else{
+						console.log(err);
+					}
+				});
+			}
+			else if(isNum){
+				data.tagIds.push(parseInt(tag));
+			}
+		});
+	}
+
+	if (data.hasOwnProperty('socialMediaTags') && !_.isEmpty(data.socialMediaTags)) {
+		if(typeof data.socialMediaTags === 'string')
+			data.socialMediaTags = new Array(data.socialMediaTags);
+		data.socialMediaTags.forEach(function(socialMediaTag, key){
+			socialMediaTag = socialMediaTag.trim();
+			var isNum = /^\d+$/.test(socialMediaTag);
+			if(!isNum && !_.isEmpty(socialMediaTag)){
+				db.query('SET  @socialMediaTagId= -1; CALL insertSocialMediaTag("'+socialMediaTag+'", @socialMediaTagId); SELECT @socialMediaTagId;', function(err, rows, fields) {
+
+					if (!err){
+						storeId(rows[2][0], 'socialMediaTag').then(function(){
+							checkInsertVideo();
+						});
+
+					}
+					else{
+						console.log(err);
+					}
+				});
+			}
+			else if(isNum){
+				data.socialMediaTagIds.push(parseInt(socialMediaTag));
+			}
+		});
+	}
+
+	function storeId(obj, checkString){
+		return new Promise(function(resolve, reject){
+			var pushed = false;
+			for(var key in obj){
+				if(checkString === 'tag'){
+					data.tagIds.push(obj[key]);
+					if(data.tags.length === data.tagIds.length){
+						flag.tags = true;
+					}
+					pushed = true;
+				}
+				else if(checkString === 'artist'){
+					data.artistIds.push(obj[key]);
+					if(data.artists.length === data.artistIds.length){
+						flag.artists = true;
+					}
+					pushed = true;
+				}
+				else if(checkString === 'socialMediaTag'){
+					data.socialMediaTagIds.push(obj[key]);
+					if(data.socialMediaTags.length === data.socialMediaTagIds.length){
+						flag.socialMediaTags = true;
+					}
+					pushed = true;
+				}
+				break;
+			}
+			if(pushed){
+				resolve();
+			}
+		});
+	}
+
+	function checkInsertVideo(){
+		if(flag.artists && flag.tags && flag.socialMediaTags){
+			if(data.hasOwnProperty('type') && !_.isEmpty(data.type) && data.hasOwnProperty('title') && !_.isEmpty(data.title)){
+				db.query('SET  @videoId= -1; CALL insertVideo("'+
+					parseInt(data.type)+
+					'","'+data.title+
+					'","'+data.description+
+					'","'+parseInt(data.team)+
+					'","'+parseInt(data.seriesId)+
+					'","'+parseInt(data.season)+
+					'","'+parseInt(data.episode)+
+					'","'+parseInt(data.ageRestriction)+
+					'","'+data.imdbUrl+
+					'","'+data.rottenTomatoesUrl+
+					'","'+data.youtubeUrl+
+					'",@videoId); SELECT @videoId;', function(err, rows, fields) {
+
+					if (!err){
+						insertMultipleMappings(rows[2][0]).then(function(videoId){
+							pageRedirect(videoId);
+						});
+
+					}
+					else{
+						console.log(err);
+					}
+				});
+			}
+		}
+	}
+
+	function pageRedirect(videoId){
+		res.redirect('/test');
+	}
+
+	function insertMultipleMappings(obj){
+		return new Promise(function(resolve, reject){
+			var inserted = {
+				artists: false,
+				genres: false,
+				tags: false,
+				socialMediaTags: false
+			};
+
+			var count = {
+				artists: 0,
+				genres: 0,
+				tags: 0,
+				socialMediaTags: 0
+			};
+
+			var videoId = -1;
+			for(var key in obj){
+				videoId = obj[key];
+				break;
+			}
+
+			if(videoId > 0 && !_.isEmpty(data.artistIds)){
+				data.artistIds.forEach(function(artist){
+					db.query('CALL insertVideoArtistMapping("'+videoId+'", "'+artist+'");', function(err, rows, fields) {
+						if(!err){
+							checkSingleInsertion('artist').then(function(){
+								checkAllInsertions();
+							});
+						}
+						else{
+							console.log(err);
+						}
+					});
+				});
+			}
+			if(videoId > 0 && !_.isEmpty(data.genres)){
+				data.genres.forEach(function(genre){
+					db.query('CALL insertVideoGenreMapping("'+videoId+'", "'+parseInt(genre)+'");', function(err, rows, fields) {
+						if(!err){
+							checkSingleInsertion('genre').then(function(){
+								checkAllInsertions();
+							});
+						}
+						else{
+							console.log(err);
+						}
+					});
+				});
+			}
+			if(videoId > 0 && !_.isEmpty(data.tagIds)){
+				data.tagIds.forEach(function(tag){
+					db.query('CALL insertVideoTagMapping("'+videoId+'", "'+tag+'");', function(err, rows, fields) {
+						if(!err){
+							checkSingleInsertion('tag').then(function(){
+								checkAllInsertions();
+							});
+						}
+						else{
+							console.log(err);
+						}
+					});
+				});
+			}
+			if(videoId > 0 && !_.isEmpty(data.socialMediaTagIds)){
+				data.socialMediaTagIds.forEach(function(socialMediaTag){
+					db.query('CALL insertVideoSocialMediaTagMapping("'+videoId+'", "'+socialMediaTag+'");', function(err, rows, fields) {
+						if(!err){
+							checkSingleInsertion('socialMediaTag').then(function(){
+								checkAllInsertions();
+							});
+						}
+						else{
+							console.log(err);
+						}
+					});
+				});
+			}
+			function checkSingleInsertion(checkString){
+				return new Promise(function(resolve, reject){
+					if(checkString === 'artist'){
+						count.artists += 1;
+						if(count.artists === data.artistIds.length){
+							inserted.artists = true;
+						}
+					}
+					else if(checkString === 'genre'){
+						count.genres += 1;
+						if(count.genres === data.genres.length){
+							inserted.genres = true;
+						}
+					}
+					else if(checkString === 'tag'){
+						count.tags += 1;
+						if(count.tags === data.tagIds.length){
+							inserted.tags = true;
+						}
+					}
+					else if(checkString === 'socialMediaTag'){
+						count.socialMediaTags += 1;
+						if(count.socialMediaTags === data.socialMediaTagIds.length){
+							inserted.socialMediaTags = true;
+						}
+					}
+
+					if(inserted.artists && inserted.genres && inserted.tags && inserted.socialMediaTags){
+						resolve();
+					}
+
+				});
+			}
+
+			function checkAllInsertions(){
+				resolve(videoId);
+			}
+
+		});
+	}
+	
+
+});
+
+app.get('/test', function(req, res){
+	res.render('test');
+});
+
+app.post('/test', function(req, res){
+	console.log(req.body.username + " is the username") ;
+	console.log(res);
+	var res = '';
+	db.connect();
+	db.query('SELECT * from genres', function(err, rows, fields) {
+	  	if (!err){
+	  		res = rows;
+	    	console.log('The solution is: ', rows);
+		}
+		else{
+		    console.log('Error while performing Query.');
+		}
+	});
+	db.end();
+	res.render('test', res);
+	// res.end('html');
+
+});
+
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 var express = require('express');
+// var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-
 
 app.set('view engine', 'ejs');
 
 var path = require ('path');
-
-app.set('views', path.join(__dirname + '/public/views'));
+app.set('views', path.join(__dirname + 'views'));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-	res.render('index');
-});
-
-app.get('/videos', function(req, res){
-	res.render('videos');
-});
-
-app.get('/videos/add', function(req, res){
-	res.render('addVideo');
-});
+app.use(require('./config/routes.js'));
 
 app.listen(PORT, function(){
 	console.log('Express server started at '+ PORT + '!');
